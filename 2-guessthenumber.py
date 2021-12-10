9b48649cab6e5e301e9d234a8631137b7e81f46e
unknown <giancarloestrella09@gmail.com>
2021-12-10 17:24:58 +0800

    Continuously asks the user to guess until the number matches.

--- 2-guessthenumber.py
+++ 2-guessthenumber.py
@@ -4,9 +4,32 @@
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
 
-import random
+from random import randrange
 min = 0
 max = 100
 
-randNum = sorted(random.sample(range(min, max)))
-userGuess = int(input("Guess the number here:"))
+
+randNum = randrange(min, max)
+
+# Initializing the number of guesses.
+count = 0
+ 
+# for calculation of minimum number of
+# guesses depends upon range
+while count >= 0:
+    count = count
+ 
+    # taking guessing number as input
+    userGuess = int(input("Pick a number from {} through {}: ".format(min, max)))
+ 
+    # Condition testing
+    if (randNum == userGuess):
+        count = count + 1
+        print(f"Congratulations! you got the generated number, {randNum}. You did it in {count} tries.")
+        # Once guessed, loop will break
+        break
+    elif (randNum > userGuess):
+        print("The number you guessed is less than the generated number. Try again.")
+    elif (randNum < userGuess):
+        print("The number you guessed is greater than the generated number. Try again.")
+    count = count + 1
